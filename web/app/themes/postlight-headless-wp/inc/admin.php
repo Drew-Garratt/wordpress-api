79d941f688f04bf2cdd6123a2c1ab24e7fa6c275
Drew Garratt <drew.garratt@bionical.com>
2018-03-27 13:14:29 +0100

    Added linting config

--- web/app/themes/postlight-headless-wp/inc/admin.php
+++ web/app/themes/postlight-headless-wp/inc/admin.php
@@ -5,12 +5,13 @@
  * This function removes that automatic reordering so the categories widget retains its order regardless of checked state.
  * Thanks to https://stackoverflow.com/a/12586404
  */
-function taxonomy_checklist_checked_ontop_filter ( $args ) {
-	$args['checked_ontop'] = false;
-	return $args;
+function taxonomy_checklist_checked_ontop_filter($args)
+{
+    $args['checked_ontop'] = false;
+    return $args;
 }
 
-add_filter( 'wp_terms_checklist_args', 'taxonomy_checklist_checked_ontop_filter' );
+add_filter('wp_terms_checklist_args', 'taxonomy_checklist_checked_ontop_filter');
 
 /**
  * Customize the preview button in the WordPress admin to point to the headless client.
@@ -18,37 +19,45 @@ add_filter( 'wp_terms_checklist_args', 'taxonomy_checklist_checked_ontop_filter'
  * @param  str $link The WordPress preview link.
  * @return str The headless WordPress preview link.
  */
-function set_headless_preview_link( $link ) {
-	return get_frontend_origin() . '/'
-		. '_preview/'
-		. get_the_ID() . '/'
-		. wp_create_nonce( 'wp_rest' );
+function set_headless_preview_link($link)
+{
+    $post_id = get_the_ID();
+    $revisions = wp_get_post_revisions($post_id);
+    $latest_revision = array_shift($revisions);
+    return get_frontend_origin() . '/'
+        . 'news/_preview/?id='
+        . $post_id . '&revision='
+        . $latest_revision->ID . '&token='
+        . wp_create_nonce('wp_rest');
 }
 
-add_filter( 'preview_post_link', 'set_headless_preview_link' );
+add_filter('preview_post_link', 'set_headless_preview_link');
 
 /**
  * Customize the link button in the WordPress admin to point to the headless client.
  */
-function change_permalinks($permalink, $post) {
-	return str_replace(env('WP_HOME'), env('FE_ORIGIN'), $permalink);
+function change_permalinks($permalink, $post)
+{
+    return str_replace(env('WP_HOME'), env('FE_ORIGIN'), $permalink);
 }
-function change_news_permalinks($permalink, $post) {
-	return str_replace(env('WP_HOME'), env('FE_ORIGIN') . '/news', $permalink);
+function change_news_permalinks($permalink, $post)
+{
+    return str_replace(env('WP_HOME'), env('FE_ORIGIN') . '/news', $permalink);
 }
 
-add_filter( 'post_link', 'change_news_permalinks', 10, 3);
-add_filter( 'page_link', 'change_permalinks', 10, 3);
-add_filter( 'post_type_link', 'change_permalinks', 10, 3);
-add_filter( 'category_link', 'change_permalinks', 11, 3);
-add_filter( 'tag_link', 'change_permalinks', 10, 3);
-add_filter( 'author_link', 'change_permalinks', 11, 3);
-add_filter( 'day_link', 'change_permalinks', 11, 3);
-add_filter( 'month_link', 'change_permalinks', 11, 3);
-add_filter( 'year_link', 'change_permalinks', 11, 3);
+add_filter('post_link', 'change_news_permalinks', 10, 3);
+add_filter('page_link', 'change_permalinks', 10, 3);
+add_filter('post_type_link', 'change_permalinks', 10, 3);
+add_filter('category_link', 'change_permalinks', 11, 3);
+add_filter('tag_link', 'change_permalinks', 10, 3);
+add_filter('author_link', 'change_permalinks', 11, 3);
+add_filter('day_link', 'change_permalinks', 11, 3);
+add_filter('month_link', 'change_permalinks', 11, 3);
+add_filter('year_link', 'change_permalinks', 11, 3);
 
-function limit_page_depth($a) {
-  $a['depth'] = 1;
-  return $a;
+function limit_page_depth($a)
+{
+    $a['depth'] = 1;
+    return $a;
 }
-add_action('page_attributes_dropdown_pages_args','limit_page_depth');
+add_action('page_attributes_dropdown_pages_args', 'limit_page_depth');
--- web/app/themes/postlight-headless-wp/inc/theme-activation.php
+++ web/app/themes/postlight-headless-wp/inc/theme-activation.php
@@ -1,41 +1,40 @@
 <?php
 //create the homepage on activation
-if (isset($_GET['activated']) && is_admin()){
+if (isset($_GET['activated']) && is_admin()) {
+    $frontpage_title = 'Front Page'; //front page title
+    $postspage_title = 'News'; //news page title
 
-	$frontpage_title = 'Front Page'; //front page title
-	$postspage_title = 'News'; //news page title
+    //don't change the code bellow, unless you know what you're doing
 
-	//don't change the code bellow, unless you know what you're doing
-
-	$frontpage_check = get_page_by_title($frontpage_title);
-	$postspage_check = get_page_by_title($postspage_title);
-	$new_frontpage = array(
-		'post_type' => 'page', 
-		'post_title' => $frontpage_title,
-		'post_content' => '',
-		'post_status' => 'publish',
-		'post_author' => 1,
-	);
-	$new_postspage = array(
-		'post_type' => 'page', 
-		'post_title' => $postspage_title,
-		'post_content' => '',
-		'post_status' => 'publish',
-		'post_author' => 1,
-	);
-	if(!isset($frontpage_check->ID)){
-		wp_insert_post($new_frontpage);
-		$homepage = get_page_by_title($frontpage_title);
-		if ( $homepage ) {
-			update_option( 'page_on_front', $homepage->ID );
-			update_option( 'show_on_front', 'page' );
-		}
-	}
-	if(!isset($postspage_check->ID)){
-		wp_insert_post($new_postspage);
-		$blog = get_page_by_title($postspage_title);	
-		if ( $blog ) {
-			update_option( 'page_for_posts', $blog->ID );
-		}
-	}
+    $frontpage_check = get_page_by_title($frontpage_title);
+    $postspage_check = get_page_by_title($postspage_title);
+    $new_frontpage = array(
+        'post_type' => 'page',
+        'post_title' => $frontpage_title,
+        'post_content' => '',
+        'post_status' => 'publish',
+        'post_author' => 1,
+    );
+    $new_postspage = array(
+        'post_type' => 'page',
+        'post_title' => $postspage_title,
+        'post_content' => '',
+        'post_status' => 'publish',
+        'post_author' => 1,
+    );
+    if (!isset($frontpage_check->ID)) {
+        wp_insert_post($new_frontpage);
+        $homepage = get_page_by_title($frontpage_title);
+        if ($homepage) {
+            update_option('page_on_front', $homepage->ID);
+            update_option('show_on_front', 'page');
+        }
+    }
+    if (!isset($postspage_check->ID)) {
+        wp_insert_post($new_postspage);
+        $blog = get_page_by_title($postspage_title);
+        if ($blog) {
+            update_option('page_for_posts', $blog->ID);
+        }
+    }
 }
